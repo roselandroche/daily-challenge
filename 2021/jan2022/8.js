937b751eb128d8c81fe579fedea38de3ebe0da81
Rose Landroche <r.landroche@gmail.com>
2022-01-08 14:41:29 -0800

    added desc

--- 2021/jan2022/8.js
+++ 2021/jan2022/8.js
@@ -1,4 +1,13 @@
 /*
+https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+
+1967. Number of Strings That Appear as Substrings in Word
+Level: Easy
+
+Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+
+A substring is a contiguous sequence of characters within a string.
+
 @param {string[]} patterns
 @param {string} word
 @return {number}
